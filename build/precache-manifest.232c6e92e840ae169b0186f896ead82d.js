f1d32a2cedee7c8da02ac7d60e5b3cac328e9cc2
Ikerbeitia <beitia.iker@gmail.com>
2020-05-15 08:07:07 +0200

    new build

---
+++ build/precache-manifest.232c6e92e840ae169b0186f896ead82d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da0788e17f6fd283d433bf19afd3c39f",
+    "url": "/CatGifs/index.html"
+  },
+  {
+    "revision": "9305138e24e63fc9455c",
+    "url": "/CatGifs/static/css/main.ba74c6ed.chunk.css"
+  },
+  {
+    "revision": "e11bcfb8e06693edcfe0",
+    "url": "/CatGifs/static/js/2.e8c36df8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/CatGifs/static/js/2.e8c36df8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9305138e24e63fc9455c",
+    "url": "/CatGifs/static/js/main.b1a204fc.chunk.js"
+  },
+  {
+    "revision": "1d8c2f45388b4d7e6330",
+    "url": "/CatGifs/static/js/runtime-main.84e31d8a.js"
+  }
+]);
